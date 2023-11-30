b293033d8940f095334849659c6cf60448a7a137
MKA <MKA@ULTOP.local>
2023-11-30 21:19:21 +0400

    Сделал модельку брендов, подключил медиа

--- sgtrade/settings.py
+++ sgtrade/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/MEDIA_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
