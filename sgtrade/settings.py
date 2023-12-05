77483acb3846d4f743371ad4d7fbc6ba90520e80
Ankarp073 <ankarp7387@gmail.com>
2023-12-05 21:27:59 +0400

    quate 05/12/23

--- sgtrade/settings.py
+++ sgtrade/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'news',
+    'quote',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
