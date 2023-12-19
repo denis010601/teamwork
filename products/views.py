ba223f8df831c0b391f10769deb68bc13b469125
Niiik27 <niiik404@gmail.com>
2023-12-19 21:19:59 +0400

    Настроил фильтр

--- products/views.py
+++ products/views.py
@@ -5,25 +5,8 @@ from products.models import Products, Category
 
 
 class ProductListView(ListView):
-    # model = Products
+    model = Products
     template_name = 'products/products_list.html'
-
-    def post(self, request, *args, **kwargs):
-        category_str = request.POST.get('category')
-        if category_str:
-            category_id = int(category_str)
-        else:
-            category_id = 0
-        if category_id > 0:
-            products_list = Products.objects.filter(category=category_id)
-            msg = f"Это фильтр категория {category_id}"
-
-        else:
-            products_list = Products.objects.all()
-            msg = f"Это просто страница {category_id}"
-        categories = Category.objects.all()
-        return render(request, template_name=self.template_name,
-                      context={'products_list': products_list, 'msg': msg, 'categories': categories})
 
     def get(self, request, *args, **kwargs):
         category_str = request.GET.get('category')
@@ -31,6 +14,14 @@ class ProductListView(ListView):
             category_id = int(category_str)
         else:
             category_id = 0
+
+        categories = Category.objects.all()
+        exists_categories = []
+        for category in categories:
+            products_list = Products.objects.filter(category=category.id)
+            if products_list:
+                exists_categories.append(category)
+
         if category_id > 0:
             products_list = Products.objects.filter(category=category_id)
             msg = f"Это фильтр категория {category_id}"
@@ -38,9 +29,13 @@ class ProductListView(ListView):
         else:
             products_list = Products.objects.all()
             msg = f"Это просто страница {category_id}"
-        categories = Category.objects.all()
-        return render(request, template_name=self.template_name,
-                      context={'products_list': products_list, 'msg': msg, 'categories': categories})
+
+
+        print(categories[0])
+
+        # context = self.get_context_data()
+        return render(request, template_namdde=self.template_name,
+                      context={'products_list': products_list, 'msg': msg, 'categories': exists_categories})
 
 class ProductDetailView(DetailView):
     model = Products
