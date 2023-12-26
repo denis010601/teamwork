479df060604fc5522cc44901a3831e4f16d15052
Ринат Мухитов <malayka_73@inbox.ru>
2023-12-26 14:19:10 +0400

    Отзыв и рейтинг. Начало

--- products/urls.py
+++ products/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
 
     path('', views.ProductListView.as_view(), name='products'),
     path('<int:pk>', views.ProductDetailView.as_view(), name='products_detail'),
-    path('<int:pk>', views.add_review, name='add_review'),
-    path('<int:pk>', views.add_review, name='add_rating'),
 
 ]
--- products/views.py
+++ products/views.py
@@ -12,49 +12,32 @@ class ProductDetailView(DetailView):
     model = Products
 
 def products_detail(request, pk):
-    product = get_object_or_404(Products, pk=pk)
-    reviews = product.reviews.all()
-    ratings = product.ratings.all()
-    average_rating = ratings.aggregate(models.Avg('value'))['value__avg']
-    if average_rating:
-        average_rating = round(average_rating, 1)
-    review_form = ReviewForm()
-    rating_form = RatingForm()
-    return render(request, 'products_detail.html', {
-        'product': product,
-        'reviews': reviews,
-        'ratings': ratings,
-        'average_rating': average_rating,
-        'review_form': review_form,
-        'rating_form': rating_form,
-    })
-
-@login_required
-def add_review(request, pk):
-    product = get_object_or_404(Products, pk=pk)
+    product = get_object_or_404(Products, id=pk)
+    review = Review.objects.filter(product=pk)
     if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.product = product
-            review.user = request.user
-            review.save()
-            return redirect('products_detail.html', pk=product.pk)
+        formr = ReviewForm(request.POST)
+        if formr.is_valid():
+            form = formr.save(commit=False)
+            form.user = request.user
+            form.product = product
+            form.save()
+            return redirect(products_detail, pk)
     else:
         form = ReviewForm()
-    return render(request, 'add_review.html', {'form': form})
+    return render(request, 'products/products_detail.html', {'product':product, 'reviews':review, 'form':form})
 
-@login_required
-def add_rating(request, pk):
-    product = get_object_or_404(Products, pk=pk)
-    if request.method == 'POST':
-        form = RatingForm(request.POST)
-        if form.is_valid():
-            rating = form.save(commit=False)
-            rating.product = product
-            rating.user = request.user
-            rating.save()
-            return redirect('products_detail', pk=product.pk)
-    else:
-        form = RatingForm()
-    return render(request, 'add_rating.html', {'form': form})
+
+# @login_required
+# def add_rating(request, pk):
+#     product = get_object_or_404(Products, pk=pk)
+#     if request.method == 'POST':
+#         form = RatingForm(request.POST)
+#         if form.is_valid():
+#             rating = form.save(commit=False)
+#             rating.product = product
+#             rating.user = request.user
+#             rating.save()
+#             return redirect('products_detail', pk=product.pk)
+#     else:
+#         form = RatingForm()
+#     return render(request, 'add_rating.html', {'form': form})
